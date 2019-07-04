8c467839f4455777cf6e31e4294e4c975eb097bd
bumsik <bumdori2@gnu.ac.kr>
2019-07-04 11:39:05 +0900

    draft scheduler implementation

--- libs/task.c
+++ libs/task.c
@@ -1,21 +1,67 @@
 #include <stdlib.h>
 #include "task.h"
 
+//target is prior to 'to'
+bool is_prior(task_t* to, task_t* target)// 'to' is try to insert, target is task in list
+{
+    if(to->sec > target->sec)
+    {
+        return true;
+    }
+    if(to->msec > target->msec)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+static inline void update_time(task_t* task)
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    time_t sec = ts.tv_sec;
+    time_t msec = ts.tv_nsec / 1000000;
+
+    msec += (time_t)task->interval;
+    if(msec >= 1000)
+    {
+        sec += 1;
+        msec -= 1000;
+    }
+    task->sec = sec;
+    task->msec = msec;
+}
+//find task which met the condition.
+task_t* find_task(struct list* tasks, task_t* task, task_cond_func cond)
+{
+    struct list_elem* prev = NULL;
+    task_t* suffice_task = NULL;
+    struct list_elem* last_elem = list_back(tasks);
+
+    for (struct list_elem* cur = list_front(tasks); 
+         cur != last_elem;
+         cur = list_next(prev))
+    {
+        task_t* draw_task = list_entry(cur, task_t, t_elem);
+        if(cond(task, draw_task))
+        {
+            suffice_task = draw_task;
+            break;
+        }
+        prev = cur;
+    }
+
+   return suffice_task; 
+}
+
 bool is_task(task_t * task)
 {
     return task->magic == TASK_MAGIC;
 }
-static inline double get_time()
-{
-    struct timespec ts;
-    clock_gettime(CLOCK_MONOTONIC, &ts);
 
-    double sec = (double)ts.tv_sec;
-    double msec = (double)(ts.tv_nsec / 1000000);
-    return sec + msec;
-}
-
-task_t* init_task(task_func func, uint16_t _interval, uint16_t _limit)
+task_t* init_task(task_func func, uint16_t _interval, uint16_t _limit,
+                  const char* task_name)
 {
     task_t* t = (task_t*)malloc(sizeof(task_t));
 
@@ -30,7 +76,9 @@ task_t* init_task(task_func func, uint16_t _interval, uint16_t _limit)
     t->magic = TASK_MAGIC;
     t->is_skipped = false;
     t->task = func;
-    t->next_run = get_time() + (double)_interval;
+    memcpy(t->task_name, task_name, 16);
+
+    update_time(t);
     
     return t;
 }
@@ -52,8 +100,13 @@ void insert_back_task(struct list* tasks, task_t* task)
 void remove_task(task_t* task)
 {
     ASSERT(NULL != task);
-    ASSERT(is_task(task));
 
+    list_remove(&task->t_elem);
+}
+
+void destroy_task(task_t* task)
+{
+    ASSERT(NULL != task);
     list_remove(&task->t_elem);
     free(task);
     task = NULL;
@@ -65,15 +118,45 @@ task_t* fetch_task(struct list* tasks)
     {
         return NULL;
     }
-    struct list_elem* next_elem = list_pop_front(tasks);
-    task_t* next = list_entry(next_elem, task_t, t_elem);
-    ASSERT(is_task(next));
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    time_t sec = ts.tv_sec, msec = ts.tv_nsec / 1000000;
+    struct list_elem* last_elem = list_back(tasks);
+    for (struct list_elem* cur = list_front(tasks); 
+         cur != last_elem;
+         cur = list_next(cur))
+    {
+        task_t* fetched = list_entry(cur, task_t, t_elem);
+        if(fetched->sec < sec)
+        {
+            return fetched;
+        }
+        else if(fetched->sec == sec)
+        {
+            if(fetched->msec <= msec)
+            {
+                return fetched;
+            }
+        }
+    }
 
-    return next;
 }
 
 void schedule(struct list* tasks, task_t* cur)
 {
     ASSERT(is_task(cur));
-    list_push_back(tasks, &cur->t_elem);
+
+    task_t* less_prior = find_task(tasks, cur, is_prior);
+    struct list_elem* before = NULL;
+
+    if(NULL == less_prior)
+    {
+        before = list_end(tasks);
+    }
+    else
+    {
+        before = &less_prior->t_elem;
+    }
+    update_time(cur);
+    list_insert(before, &cur->t_elem);
 }
--- libs/task.h
+++ libs/task.h
@@ -9,35 +9,42 @@
 
 
 typedef void (*task_func)(void);
-
 typedef struct _task_t
 {
     task_func task;
     struct list_elem t_elem;
+    char task_name[16];
     uint16_t magic;
 
     uint16_t interval; // in ms
-    uint16_t limit; // in ms 
-    double next_run;
+    uint16_t limit; // in ms
+
+    unsigned int sec;
+    unsigned int msec;
 
     bool is_skipped;
 }task_t;
 
-task_t* init_task(task_func func, uint16_t _interval, uint16_t _limit);
+typedef bool (*task_cond_func)(task_t* target, task_t* input);
+
+
+task_t* init_task(task_func func, uint16_t _interval,
+                  uint16_t _limit, const char* task_name);
 void insert_back_task(struct list* tasks, task_t* task);
 void insert_task(task_t* before, task_t* task);
 /*
 before insert     
-        +------+        +-------+     +-------+     +------+
-    <---| head |<--->...|   1   |<--->|before |<--->| tail |<--->
-        +------+        +-------+     +-------+     +------+
+        +------+        +-------+     +-------+ ...  +------+
+    <---| head |<--->...|   1   |<--->|before | <--->| tail |<--->...
+        +------+        +-------+     +-------+      +------+
 
 after insert
-        +------+        +-------+     +-------+     +------+
-    <---| head |<--->...|  task |<--->|before |<--->| tail |<--->
-        +------+        +-------+     +-------+     +------+
+        +------+        +-------+     +-------+ ...  +------+
+    <---| head |<--->...|  task |<--->|before | <--->| tail |
+        +------+        +-------+     +-------+      +------+
  */
-void remove_task(task_t* task);
+void remove_task(task_t* task); // remove from task_list
+void destroy_task(task_t* task);
 bool is_task(task_t* task);
 
 /*
