0991fd0407f226eb6f723642f1f5c13e1603b0dd
bumsik <bumdori2@gnu.ac.kr>
2019-07-09 15:34:30 +0900

    .

---
+++ src/main.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "task.h"
+#include "log.h"
+
+extern bool interrupt = false;
+static struct list main_task;
+
+void run_task()
+{
+    while(!interrupt)
+    {
+        task_t* cur = fetch_task(&main_task);
+        cur->task();
+        schedule(&main_task, cur);
+    }
+}
+
+void init_main()
+{
+    list_init(&main_task);
+    // insert tasks
+    //initialize devices
+}
