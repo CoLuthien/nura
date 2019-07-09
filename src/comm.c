c5e17479dcb14b1d1f20807ebe087f64cc1f5ca8
bumsik <bumdori2@gnu.ac.kr>
2019-07-09 15:33:07 +0900

    need test...

--- src/comm.c
+++ src/comm.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-#include <pthread.h>
 #include "comm.h"
 #define END 0b10101010
 com_t* init_com(serial_dev_t* serial)
@@ -37,27 +36,38 @@ static void recv_frm_gcs(com_t* self)
 {
     serial_dev_t* port = self->port;
 
-    uint8_t ch;
+    int ch;
     uint8_t cnt = 0;
-    uint8_t buf[64] = {0,};
+    uint8_t buf[65] = {0,};
     do
     {
         ch = port->super.read_byte(port);
         buf[cnt] = ch;
         cnt += 1;
-    } while (ch != END && cnt <= 63);
+        
+    } while ((ch != END && ch != -1) && cnt <= 63);
     // now we have to parse the received packets
     /*
         TODO!!
      */
+    buf[64] = '\0';
+    printf("%s\n", buf);
 }
 
 static void send_to_gcs(com_t* self)
 {
+    // os will handle the send. don't care.
     serial_dev_t* port = self->port;
+    size_t len = 0;
+    size_t written = 0;
     for(unsigned int i = 0; i < self->used; i++)
     {
-        port->super.write_nbyte
-                (port, strlen(self->outbound[i]), self->outbound[i]);
+        len = strlen(self->outbound[i]);
+        written = 
+            port->super.write_nbyte(port, len, self->outbound[i]);
+        if(written < len)
+        {//debugging code
+            printf("%d byte has not written to device", len - written);
+        }
     }
 }
