09b04491e6e4a1e15dcec3b04c986ecd573b72e9
bumsik <bumdori2@gnu.ac.kr>
2019-07-09 15:33:54 +0900

    just test code only

--- sched_test.c
+++ sched_test.c
@@ -15,19 +15,13 @@ struct list* my_list;
 int main()
 {
 	
-	printf("my name is ");printf("hi\n");
-	printf("!!");
 	my_list = &tasks;
-	printf("%p\n", my_list);
 	list_init(my_list);
-	printf("%p\n", my_list);
 	task_t* task1 = init_task(task_func1, 100, 100, "TASK1");
 	task_t* task2 = init_task(task_func2, 500, 500, "TASK2");
-	printf("init process is done\n");
 	insert_back_task(my_list, task1);
 	insert_back_task(my_list, task2);
 
-	printf("insert is done!!\n");
 	while(1)
 	{
 		task_t* fetched = fetch_task(my_list);
