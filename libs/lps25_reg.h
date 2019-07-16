11d28b391c4943f8b2a7b64d1b672806f9dde2fd
bumsik <bumdori2@gnu.ac.kr>
2019-07-16 16:56:34 +0900

    lps25h regmap

---
+++ libs/lps25_reg.h
@@ -0,0 +1,28 @@
+#ifndef __LPS25_REG_H
+#define __LPS25_REG_H
+
+#define REF_P_XL 0x08
+#define REF_P_L 0x09
+#define REF_P_H 0x0a
+#define BARO_WHO_AM_I 0x0f
+#define RES_CONF 0x10
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define INTR_CFG 0x24
+#define INT_SRC 0x25
+#define STAT_REG 0x27
+#define PRESS_OUT_XL 0x28
+#define PRESS_OUT_L 0x29
+#define PRESS_OUT_H 0x2A
+#define TEMP_OUT_L 0x2b
+#define TEMP_OUT_H 0x2c
+#define FIFO_CTRL 0x2e
+#define FIFO_STAT 0x2f
+#define THS_P_L 0x30
+#define THS_P_H 0x31
+#define RPDS_L 0x39
+#define RPDS_H 0x3a
+
+#endif
